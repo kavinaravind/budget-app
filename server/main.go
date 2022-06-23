216c03f33c079ca814cde544f410da1d7a4a99e9
Kavin Aravind <kvnaravind@gmail.com>
2022-06-23 14:47:26 -0400

    starter code for mongo

--- server/main.go
+++ server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	Port          = 8080
+	ConnectionURI = "mongodb://127.0.0.1:27017/"
+)
+
+var (
+	db *mongo.Database
+)
+
+func SetupMongo(ctx context.Context) error {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(ConnectionURI))
+	if err != nil {
+		return err
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	db = client.Database("budget")
+	return nil
+}
 
 func main() {
-	fmt.Println("hello world")
+	ctx := context.TODO()
+
+	err := SetupMongo(ctx)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 }
